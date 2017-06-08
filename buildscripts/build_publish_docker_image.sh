7d9b641c1ff6ec555922b5cb0f70f6f65889414e
robs <rob.swartenbroekx@gmail.com>
2017-06-08 22:01:06 +0200

    trying to fix build

--- buildscripts/build_publish_docker_image.sh
+++ buildscripts/build_publish_docker_image.sh
@@ -1,9 +1,7 @@
-cd $HOME/build/robisrob/partei-webapp/Application
+cd $HOME/build/robisrob/webapp/Application
 dotnet publish -c Release -o ./docker_workdir/app 
 cp ../buildscripts/Dockerfile ./docker_workdir
 cd docker_workdir
 docker build -t robisrob/gol .
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 docker push robisrob/partei-webapp
-
-
