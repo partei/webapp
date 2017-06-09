1fecb70c1cf8abdbfe203001687d7353b6bf72f5
robs <rob.swartenbroekx@gmail.com>
2017-06-09 18:33:00 +0200

    cleaning up dockerfile

--- buildscripts/build_publish_docker_image.sh
+++ buildscripts/build_publish_docker_image.sh
@@ -5,6 +5,6 @@ cd docker_workdir
 docker build -t robisrob/partei-webapp:$TRAVIS_BUILD_NUMBER .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push robisrob/partei-webapp:$TRAVIS_BUILD_NUMBER
-docker login --email=$HEROKU_USERNAME --username=$HEROKU_USERNAME --password=$HEROKU_AUTH_TOKEN registry.heroku.com;
+docker login -u=$HEROKU_USERNAME -p=$HEROKU_AUTH_TOKEN registry.heroku.com;
 docker tag robisrob/partei-webapp:$TRAVIS_BUILD_NUMBER registry.heroku.com/partei/web;
 docker push registry.heroku.com/partei/web;
